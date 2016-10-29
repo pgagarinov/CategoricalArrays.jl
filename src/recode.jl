97795654f9759399425f27ea2c395070170c3b6c
Milan Bouchet-Valat <nalimilan@club.fr>
2016-10-29 19:13:08 +0200

    Change API to take Vector

--- src/recode.jl
+++ src/recode.jl
@@ -1,5 +1,5 @@
 """
-    recode!(dest::AbstractArray, src::AbstractArray, pairs::Pair...; default=Nullable())
+    recode!(dest::AbstractArray, src::AbstractArray, pairs::AbstractVector{<:Pair}, default=nothing)
 
 Fill `dest` with elements from `src`, replacing those matching a key of `pairs`
 with the corresponding value.
@@ -14,7 +14,8 @@ Elements of `src` as well as values from `pairs` will be `convert`ed when possib
 on assignment.
 If an element matches more than one key, the first match is used.
 """
-function recode!(dest::AbstractArray, src::AbstractArray, pairs::Pair...; default=nothing)
+function recode!{P <: Pair}(dest::AbstractArray, src::AbstractArray,
+                       pairs::AbstractVector{P}, default=nothing)
     if length(dest) != length(src)
         error("dest and src must be of the same length (got $(length(dest)) and $(length(src)))")
     end
@@ -39,19 +40,15 @@ function recode!(dest::AbstractArray, src::AbstractArray, pairs::Pair...; defaul
     dest
 end
 
-function recode!{T}(dest::CatArray{T}, src::AbstractArray, pairs::Pair...; default=nothing)
+function recode!{T, P <: Pair}(dest::CatArray{T}, src::AbstractArray,
+                               pairs::AbstractVector{P}, default=nothing)
     if length(dest) != length(src)
         error("dest and src must be of the same length (got $(length(dest)) and $(length(src)))")
     end
 
     levs = T[p.second for p in pairs]
     if default !== nothing
-        try
-            push!(levs, default)
-        catch err
-            isa(err, MethodError) || rethrow(err)
-            throw(ArgumentError("cannot `convert` default value $(repr(default)) (of type $(typeof(default))) to type of recoded levels ($T). Choose a value compatible with type of recoded levels."))
-        end
+        push!(levs, default)
     end
 
     levels!(dest.pool, levs)
@@ -72,11 +69,12 @@ function recode!{T}(dest::CatArray{T}, src::AbstractArray, pairs::Pair...; defau
 
         # Value not in any of the pairs
         if default === nothing
+            v = src[i]
             try
-                dest[i] = src[i] # Need a dictionary lookup, and potentially adding a new level
+                dest[i] = v # Need a dictionary lookup, and potentially adding a new level
             catch err
                 isa(err, MethodError) || rethrow(err)
-                throw(ArgumentError("cannot `convert` value $(repr(src[i])) (of type $(typeof(src[i]))) to type of recoded levels ($T). This will happen when not all original levels are recoded (i.e. some are preserved) and their type is incompatible with that of recoded levels."))
+                throw(ArgumentError("cannot `convert` value $(repr(v)) (of type $(typeof(v))) to type of recoded levels ($T). This will happen when not all original levels are recoded (i.e. some are preserved) and their type is incompatible with that of recoded levels."))
             end
         else
             drefs[i] = defaultref
@@ -96,35 +94,34 @@ function recode!{T}(dest::CatArray{T}, src::AbstractArray, pairs::Pair...; defau
     dest
 end
 
-function recode!{T}(dest::CatArray{T}, src::CatArray, pairs::Pair...; default=nothing)
+function recode!{T, P<:Pair}(dest::CatArray{T}, src::CatArray,
+                             pairs::AbstractVector{P}, default=nothing)
     if length(dest) != length(src)
         error("dest and src must be of the same length (got $(length(dest)) and $(length(src)))")
     end
 
     srclevels = levels(src)
-    levs = T[p.second for p in pairs]
+    seconds = (p.second for p in pairs)
+    levs = collect(T, seconds)
     if default === nothing
         # Remove recoded levels as they won't appear in result
         firsts = (p.first for p in pairs)
-        keptlevels = similar(levs)
-        try
-            for l in keptlevels
-                if !(l in firsts || any(f -> length(f) > 1 && l in f, firsts))
+        keptlevels = Vector{T}()
+        sizehint!(keptlevels, length(srclevels))
+
+        for l in srclevels
+            if !(l in firsts || any(f -> length(f) > 1 && l in f, firsts))
+                try
                     push!(keptlevels, l)
+                catch err
+                    isa(err, MethodError) || rethrow(err)
+                    throw(ArgumentError("cannot `convert` value $(repr(l)) (of type $(typeof(l))) to type of recoded levels ($T). This will happen when not all original levels are recoded (i.e. some are preserved) and their type is incompatible with that of recoded levels."))
                 end
             end
-        catch err
-            isa(err, MethodError) || rethrow(err)
-            throw(ArgumentError("cannot `convert` value $(repr(src[i])) (of type $(typeof(src[i]))) to type of recoded levels ($T). This will happen when not all original levels are recoded (i.e. some are preserved) and their type is incompatible with that of recoded levels."))
         end
         levs, ordered = mergelevels(isordered(src), keptlevels, levs)   
     else
-        try
-            push!(levs, default)
-        catch err
-            isa(err, MethodError) || rethrow(err)
-            throw(ArgumentError("cannot `convert` default value $(repr(default)) (of type $(typeof(default))) to type of recoded levels ($T). Choose a value compatible with type of recoded levels."))
-        end
+        push!(levs, default)
         ordered = isordered(src) # FIXME: test this
     end
 
@@ -168,14 +165,14 @@ function recode!{T}(dest::CatArray{T}, src::CatArray, pairs::Pair...; default=no
 end
 
 """
-    recode!(a::AbstractArray, pairs::Pair...; default=nothing)
+    recode!(a::AbstractArray, pairs::AbstractVector{<:Pair}, default=nothing)
 
 Convenience function for in-place recoding, equivalent to `recode!(a, a, ...)`.
 
 **Example:**
 ```julia
-julia> x = collect(1:10);
-julia> recode!(x, 1=>100, 2:4=>0, [5; 9:10]=>-1);
+julia> x = collect(1:10)
+julia> recode!(x, [1=>100, 2:4=>0, [5; 9:10]=>-1]);
 julia> x
 10-element Array{Int64,1}:
  100
@@ -190,20 +187,11 @@ julia> x
   -1
   ```
 """
-recode!(a::AbstractArray, pairs::Pair...; default=nothing) =
-    recode!(a, a, pairs...; default=default)
-
-
-Base.@pure promote_pairtype(p::Pair, q::Pair) = promote_pairtype(typeof(p), typeof(q))
-Base.@pure promote_pairtype(p::Pair, q::Pair, r::Pair, s::Pair...) =
-    promote_pairtype(typeof(p), promote_pairtype(q, r, s...))
-Base.@pure promote_pairtype{S, T}(::Type{T}, ::Type{S}) =
-    Pair{promote_type(T.parameters[1], S.parameters[1]),
-         promote_type(T.parameters[2], S.parameters[2])}
-pair_valtype{K, V}(::Type{Pair{K, V}}) = V
+recode!{P<:Pair}(a::AbstractArray, pairs::AbstractVector{P}, default=nothing) =
+    recode!(a, a, pairs, default)
 
 """
-    recode(src::AbstractArray, pairs::Pair...; default=nothing)
+    recode(src::AbstractArray, pairs::AbstractVector{<:Pair}, default=nothing)
 
 Return a new `CategoricalArray` with elements from `a`, replacing elements matching a key
 of `pairs` with the corresponding value. The type of the array is chosen using `promote`
@@ -218,7 +206,7 @@ If an element matches more than one key, the first match is used.
 
 **Example:**
 ```julia
-julia> y = recode(x, 1=>100, 2:4=>0, [5; 9:10]=>-1)
+julia> y = recode(1:10, [1=>100, 2:4=>0, [5; 9:10]=>-1])
 10-element CategoricalArrays.CategoricalArray{Int64,1,UInt32}:
  100
  0  
@@ -232,18 +220,18 @@ julia> y = recode(x, 1=>100, 2:4=>0, [5; 9:10]=>-1)
  -1 
   ```
 """
-function recode(src::AbstractArray, pairs::Pair...; default=nothing)
-    # T cannot take into account default nor eltype(src), since the former isn't specialized on,
-    # and whether the latter matters is only known at compile time (all levels recoded or not)
-    T = pair_valtype(promote_pairtype(pairs...))
+function recode{A, B}(src::AbstractArray, pairs::AbstractVector{Pair{A, B}}, default=nothing)
+    # T cannot take into account eltype(src), since we can't know
+    # whether it matters at compile time (all levels recoded or not)
+    T = default === nothing ? B : promote_type(B, typeof(default))
     dest = CategoricalArray{T}(size(src))
-    recode!(dest, src, pairs...; default=default)
+    recode!(dest, src, pairs, default)
 end
 
-function recode{S, N, R}(src::CatArray{S, N, R}, pairs::Pair...; default=nothing)
-    # T cannot take into account default nor eltype(src), since the former isn't specialized on,
-    # and whether the latter matters is only known at compile time (all levels recoded or not)
-    T = pair_valtype(promote_pairtype(pairs...))
+function recode{S, N, R, A, B}(src::CatArray{S, N, R}, pairs::AbstractVector{Pair{A, B}}, default=nothing)
+    # T cannot take into account eltype(src), since we can't know
+    # whether it matters at compile time (all levels recoded or not)
+    T = default === nothing ? B : promote_type(B, typeof(default))
     dest = CategoricalArray{T, N, R}(size(src))
-    recode!(dest, src, pairs...; default=default)
+    recode!(dest, src, pairs, default)
 end
--- test/recode.jl
+++ test/recode.jl
@@ -3,79 +3,84 @@ module TestRecode
     using CategoricalArrays
     using CategoricalArrays: DefaultRefType
 
+    # Temporary: needed until this is included in supported Julia releases.
+    # Only needed for heterogeneous pair vectors (which are a corner case)
+    Base.promote_rule{A1, B1, A2, B2}(::Type{Pair{A1, B1}}, ::Type{Pair{A2, B2}}) =
+        Pair{promote_type(A1, A2), promote_type(B1, B2)}
+
     for x in (1:10, [1:10;], CategoricalArray(1:10))
         # Recoding from Int to Int
-        y = @inferred recode(x, 1=>100, 2:4=>0, [5; 9:10]=>-1)
-        @test @inferred y == [100, 0, 0, 0, -1, 6, 7, 8, -1, -1]
+        y = @inferred recode(x, [1=>100, 2:4=>0, [5; 9:10]=>-1])
+        @test y == [100, 0, 0, 0, -1, 6, 7, 8, -1, -1]
         @test typeof(y) === CategoricalVector{Int, DefaultRefType}
         @test levels(y) == [6, 7, 8, 100, 0, -1]
         @test !isordered(y)
 
         # Recoding from Int to Int, with a first value being Float64
-        y = @inferred recode(x, 1.0=>100, 2:4=>0, [5; 9:10]=>-1)
+        y = @inferred recode(x, [1.0=>100, 2:4=>0, [5; 9:10]=>-1])
         @test y == [100, 0, 0, 0, -1, 6, 7, 8, -1, -1]
         @test typeof(y) === CategoricalVector{Int, DefaultRefType}
         @test levels(y) == [6, 7, 8, 100, 0, -1]
         @test !isordered(y)
 
         # Recoding from Int to Float64 due to a second value being Float64
-        y = @inferred recode(x, 1=>100.0, 2:4=>0, [5; 9:10]=>-1)
+        y = @inferred recode(x, [1=>100.0, 2:4=>0, [5; 9:10]=>-1])
         @test y == [100, 0, 0, 0, -1, 6, 7, 8, -1, -1]
         @test typeof(y) === CategoricalVector{Float64, DefaultRefType}
         @test levels(y) == [6, 7, 8, 100, 0, -1]
         @test !isordered(y)
 
         # Recoding from Int to Int, with default
-        y = @inferred recode(x, 1=>100, 2:4=>0, [5; 9:10]=>-1, default=-10)
+        y = @inferred recode(x, [1=>100, 2:4=>0, [5; 9:10]=>-1], -10)
         @test y == [100, 0, 0, 0, -1, -10, -10, -10, -1, -1]
         @test typeof(y) === CategoricalVector{Int, DefaultRefType}
         @test levels(y) == [100, 0, -1, -10]
         @test !isordered(y)
 
-        # Recoding from Int to Int, with Float64 default
-        y = @inferred recode(x, 1=>100, 2:4=>0, [5; 9:10]=>-1, default=-10.0)
-        @test typeof(y) === CategoricalVector{Int, DefaultRefType}
+        # Recoding from Int to Float64, with Float64 default and all other values Int
+        y = @inferred recode(x, [1=>100, 2:4=>0, [5; 9:10]=>-1], -10.0)
+        @test typeof(y) === CategoricalVector{Float64, DefaultRefType}
         @test levels(y) == [100, 0, -1, -10]
         @test !isordered(y)
 
         # Recoding from Int to Any, with more than one second value (corner case)
-        y = @inferred recode(x, 1=>(100, 101), 2:4=>0, [5; 9:10]=>-1)
+        y = @inferred recode(x, [1=>(100, 101), 2:4=>0, [5; 9:10]=>-1])
         @test y == [(100, 101), 0, 0, 0, -1, 6, 7, 8, -1, -1]
         @test typeof(y) === CategoricalVector{Any, DefaultRefType}
         @test levels(y) == [6, 7, 8, (100, 101), 0, -1]
         @test !isordered(y)
 
         # Recoding from Int to String, with String default
-        y = @inferred recode(x, 1=>"a", 2:4=>"b", [5; 9:10]=>"c", default="d")
+        y = @inferred recode(x, [1=>"a", 2:4=>"b", [5; 9:10]=>"c"], "d")
         @test y == ["a", "b", "b", "b", "c", "d", "d", "d", "c", "c"]
         @test typeof(y) === CategoricalVector{String, DefaultRefType}
         @test levels(y) == ["a", "b", "c", "d"]
         @test !isordered(y)
 
         # Recoding from Int to String, with all original levels recoded
-        y = @inferred recode(x, 1=>"a", 2:4=>"b", [5; 9:10]=>"c", 6:8=>"d")
+        y = @inferred recode(x, [1=>"a", 2:4=>"b", [5; 9:10]=>"c", 6:8=>"d"])
         @test y == ["a", "b", "b", "b", "c", "d", "d", "d", "c", "c"]
         @test typeof(y) === CategoricalVector{String, DefaultRefType}
         @test levels(y) == ["a", "b", "c", "d"]
+        @test !isordered(y)
+
+        # Recoding from Int to Int/String (i.e. Any), with default String and other values Int
+        y = @inferred recode(x, [1=>100, 2:4=>0, [5; 9:10]=>-1], "x")
+        @test typeof(y) === CategoricalVector{Any, DefaultRefType}
+        @test levels(y) == [100, 0, -1, "x"]
         @test !isordered(y)
 
         # Recoding from Int to Int/String, without any Int value in pairs
         # and keeping some original Int levels
         # This must fail since we cannot take into account original eltype (whether original levels
         # are kept is only known at compile time)
-        res = @test_throws ArgumentError recode(x, 1=>"a", 2:4=>"b", [5; 9:10]=>"c")
+        res = @test_throws ArgumentError recode(x, [1=>"a", 2:4=>"b", [5; 9:10]=>"c"])
         @test sprint(showerror, res.value) == "ArgumentError: cannot `convert` value 6 (of type Int64) to type of recoded levels (String). This will happen when not all original levels are recoded (i.e. some are preserved) and their type is incompatible with that of recoded levels."
-
-        # Recoding from Int to Int/String (i.e. Any), with default String and other values Int
-        # This must fail since using default to determine eltype of result
-        # would introduce type instability
-        res = @test_throws ArgumentError recode(x, 1=>100, 2:4=>0, [5; 9:10]=>-1, default="x")
-        @test sprint(showerror, res.value) == "ArgumentError: cannot `convert` default value \"x\" (of type String) to type of recoded levels (Int64). Choose a value compatible with type of recoded levels."
     end
 
     for x in (["a", "c", "b", "a"], CategoricalArray(["a", "c", "b", "a"]))
         # Recoding from String to String
-        y = @inferred recode(x, "c"=>"x", "b"=>"y", "a"=>"z")
+        y = @inferred recode(x, ["c"=>"x", "b"=>"y", "a"=>"z"])
         @test y == ["z", "x", "y", "z"]
         @test typeof(y) === CategoricalVector{String, DefaultRefType}
         @test levels(y) == ["x", "y", "z"]
@@ -84,7 +89,7 @@ module TestRecode
 
     for x in (['a' 'c'; 'b' 'a'], CategoricalArray(['a' 'c'; 'b' 'a']))
         # Recoding a Matrix
-        y = @inferred recode(x, 'c'=>'x', 'b'=>'y', 'a'=>'z')
+        y = @inferred recode(x, ['c'=>'x', 'b'=>'y', 'a'=>'z'])
         @test y == ['z' 'x'; 'y' 'z']
         @test typeof(y) === CategoricalMatrix{Char, DefaultRefType}
         @test levels(y) == ['x', 'y', 'z']
@@ -93,14 +98,14 @@ module TestRecode
 
     for x in (10:-1:1, CategoricalArray(10:-1:1))
         # Recoding from Int to Int/String (i.e. Any), with index and levels in different orders
-        y = @inferred recode(x, 1=>"a", 2:4=>"c", [5; 9:10]=>"b")
-        @test y == ["b", "b", 8, 7, 6, "b", "c", "c", "c", "a"]
+        y = @inferred recode(x, [1=>"a", 2:4=>"c", [5; 9:10]=>"b"], 0)
+        @test y == ["b", "b", 0, 0, 0, "b", "c", "c", "c", "a"]
         @test typeof(y) === CategoricalVector{Any, DefaultRefType}
-        @test levels(y) == [6, 7, 8, "a", "c", "b"]
+        @test levels(y) == ["a", "c", "b", 0]
         @test !isordered(y)
 
         # Recoding from Int to String via default, with index and levels in different orders
-        y = @inferred recode(x, 1=>"a", 2:4=>"c", [5; 9:10]=>"b", default="x")
+        y = @inferred recode(x, [1=>"a", 2:4=>"c", [5; 9:10]=>"b"], "x")
         @test y == ["b", "b", "x", "x", "x", "b", "c", "c", "c", "a"]
         @test typeof(y) === CategoricalVector{String, DefaultRefType}
         @test levels(y) == ["a", "c", "b", "x"]
@@ -109,7 +114,7 @@ module TestRecode
 
     # Recoding CategoricalArray with custom reftype
     x = CategoricalVector{Int, UInt8}(1:10)
-    y = @inferred recode(x, 1=>100, 2:4=>0, [5; 9:10]=>-1)
+    y = @inferred recode(x, [1=>100, 2:4=>0, [5; 9:10]=>-1])
     @test y == [100, 0, 0, 0, -1, 6, 7, 8, -1, -1]
     @test typeof(y) === CategoricalVector{Int, UInt8}
     @test levels(y) == [6, 7, 8, 100, 0, -1]
@@ -118,7 +123,7 @@ module TestRecode
     # Recoding ordered CategoricalArray and merging two categories
     x = CategoricalArray(["a", "c", "b", "a"])
     ordered!(x, true)
-    y = @inferred recode(x, "c"=>"a")
+    y = @inferred recode(x, ["c"=>"a"])
     @test y == ["a", "a", "b", "a"]
     @test typeof(y) === CategoricalVector{String, DefaultRefType}
     @test levels(y) == ["a", "b"]
@@ -127,7 +132,7 @@ module TestRecode
     # Recoding ordered CategoricalArray and merging one category into another (contd.)
     x = CategoricalArray(["a", "c", "b", "a"])
     ordered!(x, true)
-    y = @inferred recode(x, "b"=>"c")
+    y = @inferred recode(x, ["b"=>"c"])
     @test y == ["a", "c", "c", "a"]
     @test typeof(y) === CategoricalVector{String, DefaultRefType}
     @test levels(y) == ["a", "c"]
@@ -136,9 +141,11 @@ module TestRecode
     # Recoding ordered CategoricalArray and breaking orderedness
     x = CategoricalArray(["a", "c", "b", "a"])
     ordered!(x, true)
-    y = @inferred recode(x, "b"=>"d")
+    y = @inferred recode(x, ["b"=>"d"])
     @test y == ["a", "c", "d", "a"]
     @test typeof(y) === CategoricalVector{String, DefaultRefType}
     @test levels(y) == ["a", "c", "d"]
     @test !isordered(y)
+
+    # TODO: test overlapping pairs, and pairs with no matches
 end
